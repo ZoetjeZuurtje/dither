b52131e426eae80be3212886dc89d614794cda3f
Bram <bramstrik@live.com>
2023-04-15 20:08:32 +0200

    Split the dithering process into a seperate file.

--- src/dithering.rs
+++ src/dithering.rs
@@ -1,23 +1,28 @@
 use image::{Luma, DynamicImage, GenericImageView, ImageBuffer};
 
+
+fn error_diffusion(buffer: &mut ImageBuffer<Luma<u8>, Vec<u8>>, x: u32, y: u32, quant_error: f32, weight: f32) {
+
+    let pixel = match buffer.get_pixel_checked(x, y) {
+        Some(pixel) => pixel[0] as f32 + quant_error * weight,
+        None => return,
+    };
+    let pixel: Luma<u8> = Luma([pixel as u8]);
+    buffer.put_pixel(x, y, pixel);
+}
+
+// Floyd-Steinberg dithering!
 pub fn floyd_steinberg(img: &DynamicImage) -> ImageBuffer<Luma<u8>, Vec<u8>> {
     let mut buffer: ImageBuffer<Luma<u8>, Vec<u8>> = img.to_luma8();
     let mut old_pixel: u8;
     let mut new_pixel: u8;
     let mut quant_error: f32;
-    let mut result: f32;
-
-    // Gets the width and height to prevent crashes due to out of bounds pixels
-    let (width, mut height) = img.dimensions();
-    height -= 1;
 
     // Iterate over the pixels
-    for (imgx, imgy, _) in img.pixels() {
+    for (imgx, imgy, pixel) in img.pixels() {
 
-        // Floyd-Steinberg dithering!
-        old_pixel = buffer.get_pixel(imgx, imgy)[0];
+        old_pixel = pixel[0];
         new_pixel = 0;
-        
         if old_pixel > 127 { new_pixel = 255 };
 
         buffer.put_pixel(imgx, imgy, Luma([new_pixel]));
@@ -25,30 +30,12 @@ pub fn floyd_steinberg(img: &DynamicImage) -> ImageBuffer<Luma<u8>, Vec<u8>> {
         quant_error = old_pixel as f32 - new_pixel as f32;
         
         // Error diffusion
-        if imgx + 1 < width {
-
-            result = buffer.get_pixel(imgx + 1, imgy)[0] as f32 + (quant_error * 0.4375);
-            if result > 255.0 { result = 255.0 };
-            buffer.put_pixel(imgx + 1, imgy, Luma([result as u8])); // 7
-
-            if imgy < height {
-                result = buffer.get_pixel(imgx + 1, imgy + 1 )[0] as f32 + (quant_error * 0.0625);
-                if result > 255.0 { result = 255.0 };
-                buffer.put_pixel(imgx + 1, imgy + 1, Luma([result as u8])); // 1
-            }
-        };
-    
-        if imgx != 0 && imgy < height {
-            result = buffer.get_pixel(imgx - 1, imgy + 1 )[0] as f32 + (quant_error * 0.3125);
-            if result > 255.0 { result = 255.0 };
-            buffer.put_pixel(imgx - 1, imgy + 1, Luma([result as u8])); // 5
-        };
-    
-        if imgy < height {
-            result = buffer.get_pixel(imgx, imgy + 1 )[0] as f32 + (quant_error * 0.1875);
-            if result > 255.0 { result = 255.0 };
-            buffer.put_pixel(imgx, imgy + 1, Luma([result as u8])); // 3
-        };
+        error_diffusion(&mut buffer, imgx + 1, imgy, quant_error, 0.4375);
+        error_diffusion(&mut buffer, imgx + 1, imgy + 1, quant_error, 0.0625);
+        if imgx != 0 {
+            error_diffusion(&mut buffer, imgx - 1, imgy + 1, quant_error, 0.3125);
+        }
+        error_diffusion(&mut buffer, imgx, imgy + 1, quant_error, 0.1875);
 
     };
 
--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,25 @@
+mod dithering;
 use std::env;
-use image::{Luma, GenericImageView, ImageBuffer};
+
+use image::DynamicImage;
 
 
 fn main() {
     // CLI stuff
     let args: Vec<String> = env::args().collect();
-    let img = image::open(&args[1]).unwrap();
-    let options = &args[1];
+    let img: DynamicImage;
+    let options = &args[2];
     let mut name = "out.png";
 
-    if options.contains('o') {
-        name = &args[2];
-    }
-
-    let mut buffer: ImageBuffer<Luma<u8>, Vec<u8>> = img.to_luma8();
-    let mut old_pixel: u8;
-    let mut new_pixel: u8;
-    let mut quant_error: f32;
-    let mut result: f32;
-
-    // Gets the width and height to prevent crashes due to out of bounds pixels
-    let (width, mut height) = img.dimensions();
-    height -= 1;
-
-    // Iterate over the pixels
-    for (imgx, imgy, _) in img.pixels() {
-
-        // Floyd-Steinberg dithering!
-        old_pixel = buffer.get_pixel(imgx, imgy)[0];
-        new_pixel = 0;
-        
-        if old_pixel > 127 { new_pixel = 255 };
-
-        buffer.put_pixel(imgx, imgy, Luma([new_pixel]));
-
-        quant_error = old_pixel as f32 - new_pixel as f32;
-        
-        // Error diffusion
-        if imgx + 1 < width {
-
-            result = buffer.get_pixel(imgx + 1, imgy)[0] as f32 + (quant_error * 0.4375);
-            if result > 255.0 { result = 255.0 };
-            buffer.put_pixel(imgx + 1, imgy, Luma([result as u8])); // 7
-
-            if imgy < height {
-                result = buffer.get_pixel(imgx + 1, imgy + 1 )[0] as f32 + (quant_error * 0.0625);
-                if result > 255.0 { result = 255.0 };
-                buffer.put_pixel(imgx + 1, imgy + 1, Luma([result as u8])); // 1
-            }
-        };
-    
-        if imgx != 0 && imgy < height {
-            result = buffer.get_pixel(imgx - 1, imgy + 1 )[0] as f32 + (quant_error * 0.3125);
-            if result > 255.0 { result = 255.0 };
-            buffer.put_pixel(imgx - 1, imgy + 1, Luma([result as u8])); // 5
-        };
-    
-        if imgy < height {
-            result = buffer.get_pixel(imgx, imgy + 1 )[0] as f32 + (quant_error * 0.1875);
-            if result > 255.0 { result = 255.0 };
-            buffer.put_pixel(imgx, imgy + 1, Luma([result as u8])); // 3
-        };
-
+    match image::open(&args[1]) {
+        Result::Ok(image) => img = image,
+        Result::Err(error) => panic!("Error: {}", error),
     };
+    if options.contains('o') {
+        name = &args[3];
+    };
+    
+    let buffer = dithering::floyd_steinberg(&img);
 
     buffer.save(name).unwrap();
 }
