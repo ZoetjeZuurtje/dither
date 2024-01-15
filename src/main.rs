90092c865d49f6dd74340a31ac13276fe3e30673
Bram <bramstrik@live.com>
2024-01-15 14:56:23 +0100

    Print the path to the output to the terminal

--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,21 @@ fn main() {
         process::exit(1);
     });
 
-    if let Err(e) = run(config) {
+    if let Err(e) = run(&config) {
         println!("Application error: {}", e);
         process::exit(1);
     }
+
+    println!("File saved at: {}", config.output)
 }
 
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
+fn run(config: &Config) -> Result<(), Box<dyn Error>> {
 
-    let img = image::open(config.file_name)?;
+    let img = image::open(&config.file_name)?;
     
     let buffer = dithering::floyd_steinberg(&img, config.shades);
 
-    buffer.save(config.output)?;
+    buffer.save(&config.output)?;
 
     Ok(())
 }
